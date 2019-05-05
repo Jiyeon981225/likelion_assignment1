61223f0c5561f9cef8dc0c97c743127dd795300d
Jiyeon981225 <wldus01225@gmail.com>
2019-05-05 13:57:29 +0000

    hi

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.utils import timezone
+
+
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField(default=' ')
+    pub_date = models.DateTimeField(default=timezone.now)
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from django.utils import timezone
+from .forms import PostForm
 
 # Create your views here.
+def home(request) :
+    posts = Post.objects.all
+    return render(request, 'blog/home.html', {'posts_list': posts})
